a39e8b05c6a9f2a1f3b619144219559ded16c846
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-15 14:58:19 -0300

    Update main.cs

--- LED/main.cs
+++ LED/main.cs
@@ -10,12 +10,12 @@ Ele não possui muitos leds, e não tem certeza se conseguirá montar o número 
 Considerando a configuração dos leds dos números abaixo, faça um algoritmo que ajude 
 João a descobrir a quantidade de leds necessário para montar o valor.
 
-                        -–––––––––––––––––––––––––––––––––––––-
-                        |  _     _  _       _   _  _   _   _  |
-                        | | | |  _| _| |_| |_  |_   | |_| |_| |
-                        | |_| | |_  _|   |  _| |_|  | |_|  _| |
-                        |                                     |
-                        -–––––––––––––––––––––––––––––––––––––-
+	-–––––––––––––––––––––––––––––––––––––-
+	|  _     _  _       _   _  _   _   _  |
+	| | | |  _| _| |_| |_  |_   | |_| |_| |
+	| |_| | |_  _|   |  _| |_|  | |_|  _| |
+	|                                     |
+	-–––––––––––––––––––––––––––––––––––––-
 
 Entrada
 A entrada contém um inteiro N, (1 ≤ N ≤ 1000) correspondente ao número de casos de teste, 
@@ -26,13 +26,13 @@ Saída
 Para cada caso de teste, imprima uma linha contendo o número de leds que João precisa 
 para montar o valor desejado, seguido da palavra "leds".
 
-                    ------------------------------------------------
-                    |  Exemplo de Entrada   |   Exemplo de Saída   |
-                    |  3                    |                      |
-                    |  115380               |   27 leds            |
-                    |  2819311              |   29 leds            |
-                    |  23456                |   25 leds            |
-                    ------------------------------------------------
+	    ------------------------------------------------
+	    |  Exemplo de Entrada   |   Exemplo de Saída   |
+	    |  3                    |                      |
+	    |  115380               |   27 leds            |
+	    |  2819311              |   29 leds            |
+	    |  23456                |   25 leds            |
+	    ------------------------------------------------
 */
 
 using System;
@@ -41,31 +41,30 @@ using System.Linq;
 
 class Program {
   public static void Main (string[] args) {
-		Console.WriteLine(" _     _  _       _   _  _   _   _ ");
-		Console.WriteLine("| | |  _| _| |_| |_  |_   | |_| |_|");
-		Console.WriteLine("|_| | |_  _|   |  _| |_|  | |_|  _|");
-		Console.WriteLine("\n");
-		
-		int[] LED = new int[10];
-		LED[0] = 6;
-		LED[1] = 2;
-		LED[2] = 5;
-		LED[3] = 5;
-		LED[4] = 4;
-		LED[5] = 5;
-		LED[6] = 6;
-		LED[7] = 3;
-		LED[8] = 7;
-		LED[9] = 6;
-		
-		Console.Write("Digite o número de casos de teste: ");
-		int n = int.Parse(Console.ReadLine());
-		
-		Console.Write("Digite um número: ");
-		List<int> lista = new List<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToList());
-		
-		Console.WriteLine("\nTeste");
-		Console.WriteLine("{0}", string.Join(" ", lista));
-		
+    Console.WriteLine(" _     _  _       _   _  _   _   _ ");
+    Console.WriteLine("| | |  _| _| |_| |_  |_   | |_| |_|");
+    Console.WriteLine("|_| | |_  _|   |  _| |_|  | |_|  _|");
+    Console.WriteLine("\n");
+    
+    int[] LED = new int[10];
+    LED[0] = 6;
+    LED[1] = 2;
+    LED[2] = 5;
+    LED[3] = 5;
+    LED[4] = 4;
+    LED[5] = 5;
+    LED[6] = 6;
+    LED[7] = 3;
+    LED[8] = 7;
+    LED[9] = 6;
+    
+    Console.Write("Digite o número de casos de teste: ");
+    int n = int.Parse(Console.ReadLine());
+    
+    Console.Write("Digite um número: ");
+    List<int> lista = new List<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToList());
+    
+    Console.WriteLine("\nTeste");
+    Console.WriteLine("{0}", string.Join(" ", lista));
   }
 }
